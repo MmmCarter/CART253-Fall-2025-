f5f54d04f97c7ce2c8d379168113bd16dda5e14d
MmmCarter <mhl1183126827@gmail.com>
2025-11-29 17:17:23 -0500

    Setup variables for variation 1

---
+++ topics/variation-jam/js/variations/variation1.js
@@ -0,0 +1,9 @@
+/**
+ * DELAY MODE
+ * This file contains the code to run *only* the delay mode variation part of the program.
+ * This keeps the stuff the menu needs to do *separate* from the rest of the program.
+ */
+
+let delayBuffer = [];
+let frogX = 300;
+let frogY = 300;
