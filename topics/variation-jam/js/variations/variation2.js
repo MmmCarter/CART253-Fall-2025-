74f19776fe3f535488000370347e87cdd34e6b7f
MmmCarter <mhl1183126827@gmail.com>
2025-11-30 21:48:27 -0500

    Setup variables for variation2

---
+++ topics/variation-jam/js/variations/variation2.js
@@ -0,0 +1,10 @@
+/**
+ * SPLIT PERCEPTION MODE
+ * This file contains the code to run *only* the split perception mode variation part of the program.
+ * This keeps the stuff the menu needs to do *separate* from the rest of the program.
+ */
+
+let mic;
+let splitX = 300;
+let splitY = 300;
+let frogColor = [100, 200, 100];
