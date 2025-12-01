06b8e4e0e99d52bf88fea21c917b0290a07550c9
MmmCarter <mhl1183126827@gmail.com>
2025-11-30 22:24:45 -0500

    variation2 added

--- topics/variation-jam/js/variations/variation2.js
+++ topics/variation-jam/js/variations/variation2.js
@@ -8,3 +8,32 @@ let mic;
 let splitX = 300;
 let splitY = 300;
 let frogColor = [100, 200, 100];
+
+function splitSetup() {
+    mic = new p5.AudioIn();
+    mic.start();
+}
+
+function splitDraw() {
+    background(10);
+
+    // X follows mouse
+    splitX = mouseX;
+
+    // Y follows mic volume
+    let vol = mic.getLevel();
+    spiltY = map(vol, 0, 0.2, height * 0.7, height * 0.3);
+
+    // Color follows key (optional)
+    fill(frogColor);
+
+    drawFrog(splitX, splitY, frogColor);
+}
+
+function splitKeyPressed(event) {
+    if (event.key === "r") frogColor = [255, 80, 80];
+    if (event.key === "g") frogColor = [80, 255, 80];
+    if (event.key === "b") frogColor = [80, 80, 255];
+}
+
+function splitMousePressed() { }
