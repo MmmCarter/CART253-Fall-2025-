288ce97c96f7fc0e79f68ed518057385881d6743
MmmCarter <mhl1183126827@gmail.com>
2025-12-04 04:10:06 -0500

    Update variation1
    A frog that follows the mouse in real time has been added as a comparison to the delayed frog.  Action tracking trail has been added for delayed frog as well.

--- topics/variation-jam/js/variations/variation1.js
+++ topics/variation-jam/js/variations/variation1.js
@@ -5,41 +5,81 @@
  */
 
 let delayBuffer = []; //array to hold past positions
-let delayAmount = 30; //frames
-let frogX = 300;
-let frogY = 300;
+let delayAmount = 25; //frames
 
+//frog that follows mouse immediately
+let realFrogX = 300;
+let realFrogY = 300;
+
+//frog that follows mouse with delay
+let delayedFrogX = 300;
+let delayedFrogY = 300;
+
+let trail = []; //stores past delayed frog positions
+let trailMax = 50; //how long the trail is
+
+/**
+ * Setup
+ */
 function delaySetup() {
     delayBuffer = []; //reset buffer
+    trail = []; //reset trail
 }
 
 function delayDraw() {
     background(30, 50, 80);
 
+    // real frog follows mouse immediately
+    realFrogX = mouseX;
+    realFrogY = mouseY;
+
     // push current mouse into buffer
-    delayBuffer.push({ x: mouseX, y: mouseY });
+    delayBuffer.push({ x: realFrogX, y: realFrogY });
 
     //when buffer is long enough, update the frog's position using the oldest position
     if (delayBuffer.length > delayAmount) {
         let oldPos = delayBuffer.shift();
-        frogX = oldPos.x;
-        frogY = oldPos.y;
+        delayedFrogX = oldPos.x;
+        delayedFrogY = oldPos.y;
+
+        //save delayed frog position for trail
+        trail.push({ x: delayedFrogX, y: delayedFrogY });
+
+        if (trail.length > trailMax) {
+            trail.shift();
+        }
     }
 
-    // draw frog at delayed position
-    drawDelayFrog(frogX, frogY);
+    // draw trail
+    noFill();
+    stroke(120, 200, 255, 160);
+    strokeWeight(4);
+    beginShape();
+    for (let i = 0; i < trail.length; i++) {
+        vertex(trail[i].x, trail[i].y);
+    }
+    endShape();
+
+    // draw frogs
+    drawFrog(realFrogX, realFrogY, color(0, 255, 180));
+    drawFrog(delayedFrogX, delayedFrogY, color(255, 200, 0));
 }
 
 function delayKeyPressed() { }
 function delayMousePressed() { }
 
 // draw the frog
-function drawDelayFrog(x, y) {
+function drawFrog(x, y, col) {
     push();
     translate(x, y);
-    fill(0, 230, 120);
+    fill(col);
+    noStroke();
     ellipse(0, 0, 40, 30);
+    fill(255);
     ellipse(-10, -10, 15, 20);
     ellipse(10, -10, 15, 20);
+    fill(0);
+    ellipse(-10, -10, 5, 5);
+    ellipse(10, -10, 5, 5);
     pop();
 }
